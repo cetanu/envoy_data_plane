d1f5fe240ec848324d59db5f28599b606a3f4961
Vasilios Syrakis <vsyrakis@atlassian.com>
2020-01-29 18:24:18 +1100

    incr version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 setup(
     name='envoy_data_plane',
-    version='0.0.4',
+    version='0.0.5',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     python_requires='>=3.7.0',
