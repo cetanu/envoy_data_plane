5ca9a72114688dd3d2fb522034d73a39765e0f74
Dzuy Pham <dpham2@atlassian.com>
2023-05-16 11:04:15 +1000

    Upgrade protos to 1.26.1
    Signed-off-by: Dzuy Pham <dpham2@atlassian.com>

--- utils/download_protobufs.py
+++ utils/download_protobufs.py
@@ -19,7 +19,7 @@ os.chdir(build_directory)
 
 utf8 = "utf-8"
 
-ENVOY_VERSION = "1.25.0"
+ENVOY_VERSION = "1.26.1"
 
 proto_include = protoc.pkg_resources.resource_filename("grpc_tools", "_proto")
 envoy = Path("./envoy")
